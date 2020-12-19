0e80ec2447ef2492f04d565b39eb30d5173a0996
Aris Christoforidis <christoforidis.aris@gmail.com>
2020-12-19 20:42:18 +0200

    TTL expiration check

--- module_manager.py
+++ module_manager.py
@@ -90,6 +90,11 @@ class ModuleManager:
         Call this when a generation changes.
         """
 
+        # Update TTL for candidate modules and delete expired ones.
+        for module_properties in self._candidate_modules:
+            should_delete = self._candidate_modules[module_properties].on_generation_increase()
+            if should_delete == True: self._candidate_modules.pop(module_properties)
+
         # Get the info list and sort the list based on the average fitness 
         # of the notable modules.
         info_list = list(self._notable_modules.values())
