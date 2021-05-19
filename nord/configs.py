dcd1750386d3ffca56840c3d98de4625c38afbf7
ge.kyriakides <ge.kyriakides@gmail.com>
2021-05-19 15:14:56 +0300

    Added Earthquake data

--- nord/configs.py
+++ nord/configs.py
@@ -7,18 +7,21 @@ from nord.neural_nets.metrics import regression_rank_correlation_with_top as rr
 
 NUM_CLASSES = {'cifar10': 10,
                'activity_recognition': 7,
+               'earthquakes': 2,
                'fashion-mnist': 10,
                'graph-nasbench3': 3,
                'graph-nasbench1': 1}
 
 INPUT_SHAPE = {'cifar10': (32, 32),
                'activity_recognition': (52,),
+               'earthquakes': (512, ),
                'fashion-mnist': (28, 28),
                'graph-nasbench3': (5, ),
                'graph-nasbench1': (5, )}
 
 CHANNELS = {'cifar10': 3,
             'activity_recognition': 3,
+            'earthquakes': 1,
             'fashion-mnist': 1,
             'graph-nasbench2': 5,
             'graph-nasbench3': 5,
@@ -26,6 +29,7 @@ CHANNELS = {'cifar10': 3,
 
 CRITERION = {'cifar10': nn.CrossEntropyLoss,
              'activity_recognition': My_CrossEntropyLoss,
+             'earthquakes': My_CrossEntropyLoss,
              'fashion-mnist': nn.CrossEntropyLoss,
              'graph-nasbench3': nn.KLDivLoss,
              'graph-nasbench2': nn.KLDivLoss,
@@ -33,6 +37,7 @@ CRITERION = {'cifar10': nn.CrossEntropyLoss,
 
 PROBLEM_TYPE = {'cifar10': 'classification',
                 'activity_recognition': 'classification',
+                'earthquakes': 'classification',
                 'fashion-mnist': 'classification',
                 'graph-nasbench2': 'ranking',
                 'graph-nasbench3': 'ranking',
@@ -40,6 +45,7 @@ PROBLEM_TYPE = {'cifar10': 'classification',
 
 METRIC = {'cifar10':  [accuracy],
           'activity_recognition': [one_hot_accuracy],
+          'earthquakes':  [accuracy],
           'fashion-mnist':  [accuracy],
           'graph-nasbench2': [br(0.5), br(0.5, 'spearman')],
           'graph-nasbench3': [br(0.5), br(0.5, 'spearman')],
@@ -49,4 +55,6 @@ DIMENSION_KEEPING = {'cifar10': 32,
                      'fashion-mnist': None,
                      'graph-nasbench3': None,
                      'graph-nasbench1': None,
-                     'activity_recognition': None}
+                     'activity_recognition': None,
+                     'earthquakes': None}
+
--- nord/data_curators/time_series.py
+++ nord/data_curators/time_series.py
@@ -5,6 +5,7 @@ import zipfile
 import numpy as np
 import pandas as pd
 import torch
+from scipy.io import arff
 from sklearn.preprocessing import LabelBinarizer
 from torch.utils.data import DataLoader, TensorDataset
 
@@ -71,3 +72,40 @@ def get_activity_recognition_data(train_batch=128, test_batch=128,
     testloader = DataLoader(testset, batch_size=test_batch, shuffle=True)
 
     return trainloader, testloader, 7
+
+
+def get_earthquake_data(train_batch=128, test_batch=128):
+
+    root = './data/earthquake_data'
+    zip_file_path = root+'/Earthquakes.zip'
+
+    def get_dataset(file):
+        data = arff.loadarff(root+'/Earthquakes_TRAIN.arff')
+        data = pd.DataFrame(data[0])
+        labels = torch.Tensor(data['target'].astype(int).values)
+        labels = labels.unsqueeze(1)
+        data = torch.Tensor(data.drop(labels=['target'], axis=1).values)
+        data = data.unsqueeze(1)
+        dataset = TensorDataset(data, labels)
+        return dataset
+
+    url = 'https://timeseriesclassification.com/Downloads/Earthquakes.zip'
+
+    if not os.path.exists(root):
+        os.makedirs(root)
+
+    if not os.path.isfile(zip_file_path):
+        print('Downloading Earthquake Data.')
+        urllib.request.urlretrieve(url, zip_file_path)
+
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(root)
+
+    trainset = get_dataset(root+'/Earthquakes_TRAIN.arff')
+    testset = get_dataset(root+'/Earthquakes_TEST.arff')
+
+    trainloader = DataLoader(trainset, batch_size=train_batch, shuffle=True)
+    testloader = DataLoader(testset, batch_size=test_batch, shuffle=True)
+
+    return trainloader, testloader, 2
+
